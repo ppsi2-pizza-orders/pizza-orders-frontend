dc9c9e639e1d0e2789003df8b9852d83919b681b
filipfonal <filipfonal@gmail.com>
2018-11-10 21:30:45 +0100

    pizza creator

--- src/app/modules/restaurant/views/pizza-creator/pizza-creator.component.ts
+++ src/app/modules/restaurant/views/pizza-creator/pizza-creator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MockIngredients } from 'src/app/shared/mock/mock-ingredients';
+import { Ingredient } from 'src/app/shared/models/Ingredient';
 
 @Component({
   selector: 'app-pizza-creator',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-creator.component.scss']
 })
 export class PizzaCreatorComponent implements OnInit {
+  public avaiableIngredients: Array<Ingredient> = [];
+  public displayIngredients: Array<Ingredient> = [];
+  public dropzoneIngredients: Array<Ingredient> = [];
+  public currentIngredient?: Ingredient;
+  private pizzaPrice = 14;
+  private ingredientsPrice = 3;
+  private currentPage = 0;
+  private itemsPerPage = 20;
 
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    Object.assign(this.avaiableIngredients, MockIngredients);
+    this.displayIngredients = this.avaiableIngredients.slice(0, this.itemsPerPage);
   }
 
+  public move(item: Ingredient, list: Array<Ingredient>): void {
+    this.remove(item, this.avaiableIngredients);
+    this.remove(item, this.displayIngredients);
+    this.remove(item, this.dropzoneIngredients);
+
+    list.push(item);
+    this.refreshPage();
+  }
+
+  public remove(item: Ingredient, list: Array<Ingredient>) {
+    if (list.indexOf(item) !== -1) {
+      list.splice(list.indexOf(item), 1);
+    }
+  }
+
+  public getPrice() {
+    return this.pizzaPrice + (this.dropzoneIngredients.length * this.ingredientsPrice);
+  }
+
+  public nextPage() {
+    const canNext = (this.currentPage * this.itemsPerPage) + this.itemsPerPage < this.avaiableIngredients.length;
+    if (canNext) {
+      this.currentPage++;
+      this.refreshPage();
+    }
+  }
+
+  public prevPage() {
+    if (this.currentPage !== 0) {
+      this.currentPage--;
+      this.refreshPage();
+    }
+  }
+
+  private refreshPage() {
+    const begin = this.currentPage * this.itemsPerPage;
+    const end = begin + this.itemsPerPage;
+    this.displayIngredients = this.avaiableIngredients.slice(begin, end);
+  }
 }
---
+++ src/app/shared/mock/mock-ingredients.ts
@@ -0,0 +1,164 @@
+export const MockIngredients = [
+    {
+        id: 1,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 2,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 3,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 4,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 5,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 6,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 7,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 8,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 9,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 10,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 11,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 12,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 13,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 14,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 15,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 16,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 17,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 18,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 19,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 20,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 21,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 22,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 23,
+        name: 'ser',
+        cost: 2,
+        img: 'assets/ser.png'
+    },
+    {
+        id: 24,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 25,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 26,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+    {
+        id: 27,
+        name: 'szynka',
+        cost: 2,
+        img: 'assets/szynka.png'
+    },
+];
