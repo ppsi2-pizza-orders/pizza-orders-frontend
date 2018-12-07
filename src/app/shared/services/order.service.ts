6ae993f829f7280edbf473f2ba91e62b5d12a15e
filipfonal <filipfonal@gmail.com>
2018-12-07 22:31:07 +0100

    store order data in local storage

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -1,50 +1,69 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Product } from '../models/IProduct';
+import { DialogService } from './dialog.service';
+
+export const STORAGE_ORDER_KEY = 'orderProducts';
+export const STORAGE_RESTAURANT_ID_KEY = 'restaurantID';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  private basket: Product[] = [];
+  private order: Product[] = [];
   private restaurantID: number;
-  private basketProducts = new BehaviorSubject<Product[]>(null);
-  private basketPreviewVisible = new BehaviorSubject<boolean>(false);
+  private orderProducts = new BehaviorSubject<Product[]>(null);
 
-  constructor() { }
-
-  public isBasketPreviewVisible() {
-    return this.basketPreviewVisible.asObservable();
+  constructor(private dialogService: DialogService) {
+    let storedProducts = localStorage.getItem(STORAGE_ORDER_KEY);
+    let storedRestaurantID = localStorage.getItem(STORAGE_RESTAURANT_ID_KEY);
+    if(storedProducts && storedRestaurantID){
+      try {
+        this.order = JSON.parse(storedProducts);
+        this.orderProducts.next(this.order);
+        this.restaurantID = parseInt(storedRestaurantID);
+      } catch{
+        this.order = [];
+      }
+    }
   }
 
-  public setBasketPreviewVisible(visible: boolean) {
-    this.basketPreviewVisible.next(visible);
+  public openOrderPreview() {
+    this.dialogService.orderPreviewDialog(this.order).subscribe(products => {
+      this.order = products;
+      this.orderProducts.next(this.order);
+      this.storeOrder();
+    });
   }
 
-  public addToBasket(product: Product, restaurant?: number) {
+  public addToOrder(product: Product, restaurant?: number) {
     if (restaurant) {
-      if (this.restaurantID !== restaurant && this.basket.length > 0) {
-        this.basket = [];
+      if (this.restaurantID !== restaurant && this.order.length > 0) {
+        this.order = [];
         this.restaurantID = restaurant;
       } else {
         this.restaurantID = restaurant;
       }
     }
-    this.basket.push(product);
-    this.basketProducts.next(this.basket);
-    this.setBasketPreviewVisible(true);
+    this.order.push(product);
+    this.orderProducts.next(this.order);
+    this.storeOrder();
+    this.openOrderPreview();
   }
 
-  public removeFromBasket(product: Product) {
-    const index = this.basket.indexOf(product);
-    this.basket.splice(index, 1);
-    this.basketProducts.next(this.basket);
-    this.setBasketPreviewVisible(true);
+  public removeFromOrder(product: Product) {
+    const index = this.order.indexOf(product);
+    this.order.splice(index, 1);
+    this.orderProducts.next(this.order);
+    this.storeOrder();
   }
 
-  public getBasketProducts() {
-    return this.basketProducts.asObservable();
+  public getOrderProducts() {
+    return this.orderProducts.asObservable();
   }
 
-
+  private storeOrder(){
+    localStorage.setItem(STORAGE_RESTAURANT_ID_KEY, this.restaurantID.toString());
+    localStorage.setItem(STORAGE_ORDER_KEY, JSON.stringify(this.order));
+  }
 }
