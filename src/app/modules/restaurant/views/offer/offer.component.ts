0436f0767ebb909b19d23d19efe6c950982d22c3
filipfonal <filipfonal@gmail.com>
2018-12-28 21:09:05 +0100

    offer page

--- src/app/modules/restaurant/views/offer/offer.component.ts
+++ src/app/modules/restaurant/views/offer/offer.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { RestaurantService } from 'src/app/core/services/restaurant.service';
-import { Restaurant } from 'src/app/core/models/Restaurant';
 
 @Component({
   selector: 'app-offer',
-  templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.scss']
+  template: `
+  <div class="app-offer">
+  <h4>Oferty</h4>
+  <hr>
+  <p>Brak dostępnych ofert.</p>
+  </div>
+  `,
+  styles: [`
+    p{
+      font-size: 1.2em;
+      color: #5f5f5f;
+    }
+  `]
 })
 export class OfferComponent implements OnInit {
 
-  public restaurant: Restaurant;
+  constructor() { }
 
-  constructor(private restaurantService: RestaurantService) { }
-
-  ngOnInit() {
-    this.restaurantService.currentRestaurant.subscribe(restaurant => {
-      this.restaurant = restaurant;
-    });
-  }
+  ngOnInit() {}
 
 }
