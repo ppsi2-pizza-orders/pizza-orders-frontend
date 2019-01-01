d1bac3d2e1e6cf11c2d80eb4ae40645e02012c2e
filipfonal <filipfonal@gmail.com>
2019-01-01 23:06:23 +0100

    restaurant products view

--- src/app/modules/management/views/restaurant-menu/restaurant-menu.component.ts
+++ src/app/modules/management/views/restaurant-menu/restaurant-menu.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Restaurant, RestaurantService, Pizza } from 'src/app/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-restaurant-menu',
   templateUrl: './restaurant-menu.component.html',
   styleUrls: ['./restaurant-menu.component.scss']
 })
-export class RestaurantMenuComponent implements OnInit {
+export class RestaurantMenuComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public restaurant: Restaurant;
+  private subscription: Subscription;
+
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit() {
+    this.subscription = this.restaurantService.currentRestaurant.subscribe(restaurant => this.restaurant = restaurant);
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  add() {
+
+  }
+
+  edit(pizza: Pizza) {
+
+  }
+
 
 }
