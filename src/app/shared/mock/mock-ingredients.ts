0691996137e7aff4e9c9698c2131f4d9a18f1759
filipfonal <filipfonal@gmail.com>
2019-01-04 01:45:00 +0100

    changes in pizza crator

--- src/app/shared/mock/mock-ingredients.ts
+++ src/app/shared/mock/mock-ingredients.ts
@@ -1,164 +1,164 @@
 export const MockIngredients = [
     {
         id: 1,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 2,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 3,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 4,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 5,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 6,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 7,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 8,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 9,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 10,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 11,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 12,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 13,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 14,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 15,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 16,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 17,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 18,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 19,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 20,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 21,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 22,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 23,
-        name: 'ser',
+        name: 'salami',
         cost: 2,
-        img: 'assets/ser.png'
+        img: 'assets/salami.png'
     },
     {
         id: 24,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 25,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 26,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
     {
         id: 27,
-        name: 'szynka',
+        name: 'pieczarki',
         cost: 2,
-        img: 'assets/szynka.png'
+        img: 'assets/pieczarki.png'
     },
 ];
