f734629bb692010390bfbff20c1f701b690a8e1f
filipfonal <filipfonal@gmail.com>
2019-01-05 00:14:42 +0100

    change ingredients and pizza images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,17 +20,17 @@ export class AppComponent implements OnInit {
     if (!this.authService.isAuthenticated() && this.authService.tokenExists()) {
       this.authService.refreshToken().subscribe();
     }
-    // window.Echo = new Echo({
-    //   broadcaster: 'pusher',
-    //   key: environment.pusherKey,
-    //   wsHost: 'api.pizzaorders.pl',
-    //   authEndpoint: 'https://api.pizzaorders.pl/broadcasting/auth',
-    //   wsPort: 6001,
-    //   wssPort: 6001,
-    //   disableStats: false
-    // });
-    // window.Echo.connector.pusher.config.auth.headers = {
-    //   'Authorization': 'Bearer ' + localStorage.getItem('token')
-    // };
+    window.Echo = new Echo({
+      broadcaster: 'pusher',
+      key: environment.pusherKey,
+      wsHost: 'api.pizzaorders.pl',
+      authEndpoint: 'https://api.pizzaorders.pl/broadcasting/auth',
+      wsPort: 6001,
+      wssPort: 6001,
+      disableStats: false
+    });
+    window.Echo.connector.pusher.config.auth.headers = {
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    };
   }
 }
