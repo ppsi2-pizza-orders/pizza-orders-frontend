e451f0aa7bf1efd26c5e27041d251e5c19c27b34
Filip Fonal <filipfonal@gmail.com>
2019-01-16 22:00:46 +0100

    emloyees management

--- src/app/modules/management/views/employees/employees.component.ts
+++ src/app/modules/management/views/employees/employees.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RestaurantRoles } from 'src/app/core/utils';
+import { RestaurantService, SnackBarService, Restaurant } from 'src/app/core';
+import { Subscription } from 'rxjs';
+
+export class Grant {
+  email: string;
+  role: number;
+}
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss']
 })
-export class EmployeesComponent implements OnInit {
+export class EmployeesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public restaurantID: number;
+  public grant = new Grant();
+  public roles = [
+    { name: 'Współwłaściciel', index: RestaurantRoles.Owner },
+    { name: 'Menadżer', index: RestaurantRoles.Manager },
+    { name: 'Kucharz', index: RestaurantRoles.Cook }
+  ];
+  private subscribtion: Subscription;
+
+  constructor(private restaurantService: RestaurantService,
+    private snackBarService: SnackBarService) { }
 
   ngOnInit() {
+    this.subscribtion = this.restaurantService.currentRestaurant
+    .subscribe(restaurant => {
+      this.restaurantID = restaurant.id;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscribtion.unsubscribe();
+  }
+
+  onAddEmployee() {
+    this.restaurantService.grantUserRole(this.grant.email, this.grant.role, this.restaurantID)
+    .subscribe(() => {
+      this.snackBarService.show('Dodano użytkownika!');
+    },
+    (err) => {
+      this.snackBarService.show('Nie mona dodać tego użytkownika!');
+    });
+  }
+
+  onRemoveEmployee() {
   }
 
 }
